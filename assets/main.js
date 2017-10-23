d474bf035e23f6c2d2d76174264add44f86a468e
Kalalau's Blade <cantrellkalalau@gmail.com>
2017-10-23 01:20:30 +0100

    added knockout rendered nicknames

--- assets/main.js
+++ assets/main.js
@@ -3,44 +3,56 @@ const model = {
 		cat1: {
 			name: 'Cat 1',
 			url: 'dist/images/cat_peek-800_large_1x.jpg',
-			clicks: 0
+			clicks: 0,
+			nicknames: ['fuzzy', 'wuzzy']
 		},
 		cat2: {
 			name: 'Cat 2',
 			url: 'dist/images/cat_fuzzy-800_large_1x.jpg',
-			clicks: 0
+			clicks: 0,
+			nicknames: ['kitty', 'ditty']
 		},
 		cat3: {
 			name: 'Cat 3',
 			url: 'dist/images/cat_glasses-800_large_1x.jpg',
-			clicks: 0
+			clicks: 0,
+			nicknames: ['whiny', 'tiny']
 		},
 		cat4: {
 			name: 'Cat 4',
 			url: 'dist/images/cat_sleep-800_large_1x.jpg',
-			clicks: 0
+			clicks: 0,
+			nicknames: ['jumpy', 'lumpy']
 		},
 		cat5: {
 			name: 'Cat 5',
 			url: 'dist/images/cat_woods-800_large_1x.jpg',
-			clicks: 0
+			clicks: 0,
+			nicknames: ['smelly', 'belly']
 		}
 	},
 	isAdminVisible: false
 };
 
 const KO_Model = {
-	clicks: ko.observable(0)
+	clicks: ko.observable(model.allCats.cat1.clicks),
+	nicknames: ko.observable(model.allCats.cat1.nicknames)
 };
 
 function ViewModel() {
 	this.level = ko.computed(function() {
-		if (KO_Model.clicks() > 5) {
-			return "Level: AWESOME CAT";
-		} else {
-			return "Level: COOL CAT";
+		switch (true) {
+			case KO_Model.clicks() > 10:
+				return "Level 3: ULTRA CAT";
+			case KO_Model.clicks() > 5:
+				return "Level 2: AWESOME CAT";
+			default:
+				return "Level 1: COOL CAT";
 		}
-	}, this)
+	}, this);
+	this.nicknames = ko.computed(function() {
+		return KO_Model.nicknames()
+	});
 }
 
 const octopus = {
@@ -88,6 +100,7 @@ const octopus = {
 	setCurrentCat: function(key = 'cat1') {
 		model.currentCat = key;
 		KO_Model.clicks(model.allCats[model.currentCat].clicks);
+		KO_Model.nicknames(model.allCats[model.currentCat].nicknames);
 	},
 	getCurrentCat: function() {
 		return model.currentCat;
