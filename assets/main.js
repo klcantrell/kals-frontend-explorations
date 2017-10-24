963556e40ef93b8f8d76ebd0ccc75511437f0c5c
Kalalau L. Cantrell <kcantrell@sensorytechnologies.local>
2017-10-24 21:02:28 +0100

    refactored Cat constructor to take in data and create mutliple cats for viewmModel

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,40 @@
-const Cat = function() {
-	this.name = ko.observable('Cat 1');
-	this.imgSrc = ko.observable('dist/images/cat_peek-800_large_1x.jpg');
-	this.clickCount = ko.observable(0);
+const initialData = {
+	cat1: {
+		name: 'Cat 1',
+		imgSrc: 'dist/images/cat_peek-800_large_1x.jpg',
+		clickCount: 0,
+		nicknames: ['fuzzy', 'wuzzy']
+	},
+	cat2: {
+		name: 'Cat 2',
+		imgSrc: 'dist/images/cat_fuzzy-800_large_1x.jpg',
+		clickCount: 0,
+		nicknames: ['kitty', 'ditty']
+	},
+	cat3: {
+		name: 'Cat 3',
+		imgSrc: 'dist/images/cat_glasses-800_large_1x.jpg',
+		clickCount: 0,
+		nicknames: ['whiny', 'tiny']
+	},
+	cat4: {
+		name: 'Cat 4',
+		imgSrc: 'dist/images/cat_sleep-800_large_1x.jpg',
+		clickCount: 0,
+		nicknames: ['jumpy', 'lumpy']
+	},
+	cat5: {
+		name: 'Cat 5',
+		imgSrc: 'dist/images/cat_woods-800_large_1x.jpg',
+		clickCount: 0,
+		nicknames: ['smelly', 'belly']
+	}
+};
+
+const Cat = function(data) {
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.clickCount = ko.observable(data.clickCount);
 	this.title = ko.computed(function() {
 		let title,
 			clicks = this.clickCount();
@@ -18,16 +51,24 @@ const Cat = function() {
 		}
 		return title;
 	}, this);
-	this.nicknames = ko.observableArray(['fuzzy', 'wuzzy']);
+	this.nicknames = ko.observableArray(data.nicknames);
 };
 
 
 const ViewModel = function() {
-	this.initVal = function() {
-		alert('sup');
-	};
-	this.currentCat = ko.observable(new Cat());
-	this.incrementCounter = function() {
+	let self = this;
+
+	self.catList = ko.observableArray([]);
+	for (let i = 0, dataKeys = Object.keys(initialData); i < dataKeys.length; i++) {
+		self.catList.push(new Cat(initialData[dataKeys[i]]));
+	}
+
+	console.log(self.catList()[0]);
+	self.currentCat = ko.observable(self.catList()[0]);
+
+
+	// binding context is currentCat
+	self.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	};
 };
@@ -36,55 +77,6 @@ const ViewModel = function() {
 
 ko.applyBindings(new ViewModel());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const model = {
-// 	allCats: {
-// 		cat1: {
-// 			name: 'Cat 1',
-// 			url: 'dist/images/cat_peek-800_large_1x.jpg',
-// 			clicks: 0,
-// 			nicknames: ['fuzzy', 'wuzzy']
-// 		},
-// 		cat2: {
-// 			name: 'Cat 2',
-// 			url: 'dist/images/cat_fuzzy-800_large_1x.jpg',
-// 			clicks: 0,
-// 			nicknames: ['kitty', 'ditty']
-// 		},
-// 		cat3: {
-// 			name: 'Cat 3',
-// 			url: 'dist/images/cat_glasses-800_large_1x.jpg',
-// 			clicks: 0,
-// 			nicknames: ['whiny', 'tiny']
-// 		},
-// 		cat4: {
-// 			name: 'Cat 4',
-// 			url: 'dist/images/cat_sleep-800_large_1x.jpg',
-// 			clicks: 0,
-// 			nicknames: ['jumpy', 'lumpy']
-// 		},
-// 		cat5: {
-// 			name: 'Cat 5',
-// 			url: 'dist/images/cat_woods-800_large_1x.jpg',
-// 			clicks: 0,
-// 			nicknames: ['smelly', 'belly']
-// 		}
-// 	},
-// 	isAdminVisible: false
-// };
 
 
 
