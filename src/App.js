d114d168783f90df9cd688dde7d49ae74fbb5f31
Kalalau <cantrellkalalau@gmail.com>
2018-05-09 10:27:34 -0400

    components as children for other components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      txt: "this is the state txt",
-      cat: 0
-    };
-  }
+  state = {
+    txt: "this is the state text"
+  };
 
-  update(e) {
+  update = e => {
     this.setState({ txt: e.target.value });
-  }
+  };
 
   render() {
     return (
       <div>
-        <input onChange={this.update.bind(this)} />
-        <h1>
-          {this.state.txt} - {this.state.cat}
-        </h1>
+        <Widget update={this.update} />
+        <h1>{this.state.txt}</h1>
       </div>
     );
   }
 }
+
+const Widget = props => <input onChange={props.update} />;
 
 export default App;
