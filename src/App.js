cd526d0ef7b95c75f77a3075e966ce72c2c223b3
Kalalau <cantrellkalalau@gmail.com>
2018-05-09 10:00:52 -0400

    props + proptypes, default props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 /* Need to return a single element in JSX,
 otherwise it's the equivalent of returning a function
@@ -6,12 +7,22 @@ and then another function */
 
 class App extends React.Component {
   render() {
+    const txt = this.props.txt;
     return (
       <div>
-        <h1>Hello World</h1> <p>Hi</p>
+        <h1>{txt}</h1>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  txt: PropTypes.string.isRequired,
+  cat: PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+  txt: "this is the default txt"
+};
 
 export default App;
