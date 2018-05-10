7d13965be0f2351a4b82855e00ff4120f6135987
Kalalau <cantrellkalalau@gmail.com>
2018-05-09 20:02:51 -0400

    about the ref prop

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React from "react";
 
 class App extends React.Component {
-  state = { currentEvent: "---" };
-
+  state = { a: "" };
   update = e => {
-    this.setState({ currentEvent: e.type });
+    this.setState({
+      a: this.a.refs.input.value,
+      b: this.refs.b.value
+    });
   };
 
   render() {
     return (
       <div>
-        <textarea
-          cols="30"
-          rows="10"
-          onKeyPress={this.update}
-          onCut={this.update}
-          onCopy={this.update}
-          onPaste={this.update}
-          onBlur={this.update}
-          onFocus={this.update}
-          onDoubleClick={this.update}
-          onTouchMove={this.update}
-          onTouchEnd={this.update}
-          onTouchStart={this.update}
-        />
-        <h1>{this.state.currentEvent}</h1>
+        <Input ref={component => (this.a = component)} update={this.update} />
+        {this.state.a}
+        <hr />
+        <input ref="b" type="text" onChange={this.update} />
+        {this.state.b}
+      </div>
+    );
+  }
+}
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="input" type="text" onChange={this.props.update} />
       </div>
     );
   }
