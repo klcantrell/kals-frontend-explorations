6be7162d0d9e306e3826b0002e58ab326e11ca04
klcantrell <cantrellkalalau@gmail.com>
2018-06-03 13:19:08 -0400

    implemented a store from scratch

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import { createStore } from 'redux';
-
 const initialState = {
   count: 0,
 };
@@ -18,6 +16,29 @@ const counter = (state = initialState, action) => {
   }
 };
 
+const createStore = reducer => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.length && listeners.forEach(listener => listener());
+  };
+
+  const subscribe = listener => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
+
 const store = createStore(counter);
 
 const render = () => {
@@ -25,9 +46,13 @@ const render = () => {
   document.getElementById('countDisplay').textContent = count;
 };
 
-store.subscribe(render);
+const unsubscribeRender = store.subscribe(render);
 render();
 
 document.getElementById('inc').addEventListener('click', () => {
   store.dispatch({ type: 'INCREMENT' });
+});
+
+document.getElementById('unsub').addEventListener('click', () => {
+  unsubscribeRender();
 });
