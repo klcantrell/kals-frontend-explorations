c0b54ea722d7a5c201e1adbe189255b64c521ca7
Kalalau <cantrellkalalau@gmail.com>
2018-06-04 18:37:18 -0400

    refactor todos reducer to be composed with a smaller todo reducer

--- todosReducer.js
+++ todosReducer.js
@@ -1,16 +1,29 @@
 import initialState from './todosInitialState';
 
+const todoReducer = (state, action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return { id: action.id, text: action.text, completed: false };
+    case 'TOGGLE_TODO':
+      if (state.id === action.id) {
+        return { ...state, completed: !state.completed };
+      }
+      return state;
+    default:
+      return state;
+  }
+};
+
 const todosReducer = (state=initialState, action) => {
   switch(action.type) {
     case 'ADD_TODO':
-      const { id, text } = action;
-      return [ ...state, {id, text, completed: false} ]
+      return [ ...state, todoReducer(undefined, action) ];
     case 'TOGGLE_TODO':
       return state.map(todo => {
-        return todo.id === action.id ?
-          { ...todo, completed: !todo.completed } :
-          todo;
-      })
+        return todoReducer(todo, action);
+      });
+    default:
+      return state;
   }
 };
 
