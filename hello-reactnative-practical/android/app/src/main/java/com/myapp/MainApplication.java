bd2d949b8b70adf4a47c49705da5cab1289b5d4a
Kalalau <cantrellkalalau@gmail.com>
2018-10-14 00:13:11 -0400

    revert to before RNN V2

--- hello-reactnative-practical/android/app/src/main/java/com/myapp/MainApplication.java
+++ hello-reactnative-practical/android/app/src/main/java/com/myapp/MainApplication.java
@@ -9,40 +9,39 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.oblador.vectoricons.VectorIconsPackage;
 
-import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.reactnativenavigation.react.ReactGateway;
-
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends NavigationApplication {
-  @Override
-  protected ReactGateway createReactGateway() {
-      ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
-          @Override
-          protected String getJSMainModuleName() {
-              return "index";
-          }
-      };
-      return new ReactGateway(this, isDebug(), host);
-  }
+public class MainApplication extends Application implements ReactApplication {
 
-  @Override
-  public boolean isDebug() {
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
-  }
+    }
 
-  protected List<ReactPackage> getPackages() {
-      // Add additional packages you require here
-      // No need to add RnnPackage and MainReactPackage
+    @Override
+    protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
           new VectorIconsPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
   }
 
   @Override
-  public List<ReactPackage> createAdditionalReactPackages() {
-      return getPackages();
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
--- hello-reactnative-practical/shared/index.js
+++ hello-reactnative-practical/shared/index.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import configureStore from './store/configureStore';
 import App from './App';
 
-class HybridApp extends Component {
-  render() {
-    return <App />;
-  }
-}
+const store = configureStore();
 
-export default HybridApp;
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+export default RNRedux;
