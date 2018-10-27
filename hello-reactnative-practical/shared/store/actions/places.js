aee33593c3e78d4c5ed79ec6f0fd3cb01299a78f
Kalalau <cantrellkalalau@gmail.com>
2018-10-26 22:01:43 -0400

    initial setup for firebase

--- hello-reactnative-practical/shared/store/actions/places.js
+++ hello-reactnative-practical/shared/store/actions/places.js
@@ -1,10 +1,26 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
+import { DATABASE_URL } from 'react-native-dotenv';
 
-export const addPlace = (placeName, location) => {
-  return {
-    type: ADD_PLACE,
-    payload: { placeName, location },
+export const addPlace = (placeName, location, image) => {
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location,
+    };
+    fetch(`${DATABASE_URL}/places.json`, {
+      method: 'POST',
+      body: JSON.stringify(placeData),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => console.log(err));
   };
+  // return {
+  //   type: ADD_PLACE,
+  //   payload: { placeName, location, image },
+  // };
 };
 
 export const deletePlace = placeKey => {
