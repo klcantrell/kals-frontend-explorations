b5811d6c205ce77f8e0974d6e046776ef95e54cd
Kalalau <cantrellkalalau@gmail.com>
2018-10-31 20:52:11 -0400

    basic signUp with firebase

--- hello-reactnative-practical/shared/store/actions/auth.js
+++ hello-reactnative-practical/shared/store/actions/auth.js
@@ -1,8 +1,33 @@
+import { API_KEY } from 'react-native-dotenv';
 import { TRY_AUTH } from './actionTypes';
 
 export const tryAuth = authData => {
-  return {
-    type: TRY_AUTH,
-    payload: authData,
+  return dispatch => {
+    dispatch(signUp(authData));
+  };
+};
+
+export const signUp = authData => {
+  const url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${API_KEY}`;
+  return dispatch => {
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+        alert('Something went wrong, please try again');
+      });
   };
 };
