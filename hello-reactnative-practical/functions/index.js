20b5b795ed72df25c1d5146bd16d053d9e1ba266
klcantrell <cantrellkalalau@gmail.com>
2018-11-17 21:23:46 -0500

    protect the image upload cloud function

--- hello-reactnative-practical/functions/index.js
+++ hello-reactnative-practical/functions/index.js
@@ -15,38 +15,59 @@ const bucket = admin.storage().bucket();
 
 exports.storeImage = functions.https.onRequest((request, response) => {
   return cors(request, response, () => {
-    const uuid = UUID();
-    const body = JSON.parse(request.body);
-    fs.writeFileSync('/tmp/uploaded-image.jpg', body.image, 'base64', err => {
-      console.log(err);
-      return response.status(500).json({ error: err });
-    });
-    return bucket.upload(
-      '/tmp/uploaded-image.jpg',
-      {
-        uploadType: 'media',
-        destination: `/places/${uuid}.jpg`,
-        metadata: {
-          contentType: 'image/jpeg',
-        },
-      },
-      (err, file) => {
-        if (!err) {
-          return file
-            .getSignedUrl({
-              action: 'read',
-              expires: '01/01/3000',
-            })
-            .then(signedUrls => {
-              return response.status(201).json({
-                imageUrl: signedUrls[0],
-              });
-            });
-        } else {
-          console.log(err);
-          return response.status(500).json({ error: err });
-        }
-      }
-    );
+    if (
+      !request.headers.authorization ||
+      !request.headers.authorization.startsWith('Bearer ')
+    ) {
+      console.log('No token present');
+      return response.status(403).json({ error: 'Unauthorized' });
+    }
+    const idToken = request.headers.authorization.split('Bearer ')[1];
+    admin
+      .auth()
+      .verifyIdToken(idToken)
+      .then(decodedToken => {
+        const uuid = UUID();
+        const body = JSON.parse(request.body);
+        fs.writeFileSync(
+          '/tmp/uploaded-image.jpg',
+          body.image,
+          'base64',
+          err => {
+            console.log(err);
+            return response.status(500).json({ error: err });
+          }
+        );
+        return bucket.upload(
+          '/tmp/uploaded-image.jpg',
+          {
+            uploadType: 'media',
+            destination: `/places/${uuid}.jpg`,
+            metadata: {
+              contentType: 'image/jpeg',
+            },
+          },
+          (err, file) => {
+            if (!err) {
+              return file
+                .getSignedUrl({
+                  action: 'read',
+                  expires: '01/01/3000',
+                })
+                .then(signedUrls => {
+                  return response.status(201).json({
+                    imageUrl: signedUrls[0],
+                  });
+                });
+            } else {
+              console.log(err);
+              return response.status(500).json({ error: err });
+            }
+          }
+        );
+      })
+      .catch(err => {
+        return response.status(403).json({ error: 'Unauthorized' });
+      });
   });
 });
