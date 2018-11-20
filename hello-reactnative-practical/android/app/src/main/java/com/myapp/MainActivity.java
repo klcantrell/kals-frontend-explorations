7ed0de90b365a2311e203f21c7ed0e4364e3f1a0
klcantrell <cantrellkalalau@gmail.com>
2018-11-19 21:17:27 -0500

    add a splash screen for android

--- hello-reactnative-practical/android/app/src/main/java/com/myapp/MainActivity.java
+++ hello-reactnative-practical/android/app/src/main/java/com/myapp/MainActivity.java
@@ -4,6 +4,12 @@ import com.reactnativenavigation.controllers.SplashActivity;
 import com.imagepicker.permissions.OnImagePickerPermissionsCallback;
 import com.facebook.react.modules.core.PermissionListener;
 
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 public class MainActivity extends SplashActivity implements OnImagePickerPermissionsCallback  {
   private PermissionListener listener;
 
@@ -21,5 +27,22 @@ public class MainActivity extends SplashActivity implements OnImagePickerPermiss
       listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+  }
+
+  @Override
+  public LinearLayout createSplashLayout() {
+    LinearLayout view = new LinearLayout(this);
+    TextView textView = new TextView(this);
+
+    view.setBackgroundColor(Color.parseColor("#521751"));
+    view.setGravity(Gravity.CENTER);
+
+    textView.setTextColor(Color.parseColor("#fa923f"));
+    textView.setText("Awesome Places");
+    textView.setGravity(Gravity.CENTER);
+    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+    view.addView(textView);
+    return view;
   }
 }
