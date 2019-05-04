955dfb3b1c1ca2eec280049eb8613ec90ba81799
klcantrell <cantrellkalalau@gmail.com>
2019-05-03 21:48:05 -0400

    max-pwa: exploring cacheable assets

--- max-pwa/PWAGram/public/src/js/feed.js
+++ max-pwa/PWAGram/public/src/js/feed.js
@@ -3,6 +3,7 @@ const createPostArea = document.querySelector('#create-post');
 const closeCreatePostModalButton = document.querySelector(
   '#close-create-post-modal-btn'
 );
+const sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
@@ -27,3 +28,30 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+function createCard() {
+  const cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  const cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+  const cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = 'San Francisco Trip';
+  cardTitle.appendChild(cardTitleTextElement);
+  const cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
+  cardSupportingText.textContent = 'In San Francisco';
+  cardSupportingText.style.textAlign = 'center';
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+// fake dynamic content
+fetch('https://httpbin.org/get')
+  .then(res => res.json())
+  .then(data => createCard());
