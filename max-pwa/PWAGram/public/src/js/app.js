2a4611d869b1744242fbc120d881662204cd6bcc
klcantrell <cantrellkalalau@gmail.com>
2019-05-03 20:47:50 -0400

    max-pwa: add polyfills for fetch and promise

--- max-pwa/PWAGram/public/src/js/app.js
+++ max-pwa/PWAGram/public/src/js/app.js
@@ -1,9 +1,12 @@
 let deferredPrompt;
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(() => {
-    console.log('service worker registered');
-  });
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => {
+      console.log('service worker registered');
+    })
+    .catch(err => console.log(err));
 }
 
 window.addEventListener('beforeinstallprompt', e => {
