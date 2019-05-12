b71c2c25321e62f9a34ba99407b94548d30f562c
klcantrell <cantrellkalalau@gmail.com>
2019-05-11 22:18:30 -0400

    moore-fp: add update and "app" function

--- moore-functional-js/fpjs/counter/src/index.js
+++ moore-functional-js/fpjs/counter/src/index.js
@@ -5,20 +5,41 @@ const { div, button } = hh(h);
 
 const initModel = 0;
 
-function view(model) {
+function view(dispatch, model) {
   return div([
     div({ className: 'mv2' }, `Count: ${model}`),
-    button(
-      { className: 'pv1 ph2 mr2', onclick: () => console.log('+ clicked!') },
-      '+'
-    ),
-    button(
-      { className: 'pv1 ph2', onclick: () => console.log('- clicked!') },
-      '-'
-    ),
+    button({ className: 'pv1 ph2 mr2', onclick: () => dispatch('plus') }, '+'),
+    button({ className: 'pv1 ph2', onclick: () => dispatch('minus') }, '-'),
   ]);
+}
+
+function update(message, model) {
+  switch (message) {
+    case 'plus':
+      return model + 1;
+    case 'minus':
+      return model - 1;
+    default:
+      return model;
+  }
+}
+
+// impure code below
+function app(initModel, update, view, node) {
+  let model = initModel;
+  let currentView = view(dispatch, model);
+  node.appendChild(currentView);
+
+  function dispatch(message) {
+    model = update(message, model);
+    const updatedView = view(dispatch, model);
+    node.replaceChild(updatedView, currentView);
+    currentView = updatedView;
+  }
 }
 
 const rootNode = document.getElementById('app');
 
-rootNode.appendChild(view(initModel));
+app(initModel, update, view, rootNode);
+
+// rootNode.appendChild(view(initModel));
