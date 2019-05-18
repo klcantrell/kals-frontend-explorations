60d3f59698229f1919194f40f912683cd3fe4b2f
klcantrell <cantrellkalalau@gmail.com>
2019-05-17 22:15:48 -0400

    moore-fp: making updates from input changes

--- moore-functional-js/fpjs/caloriecounter/src/Update.js
+++ moore-functional-js/fpjs/caloriecounter/src/Update.js
@@ -1,5 +1,9 @@
+import * as R from 'ramda';
+
 const MSGS = {
   SHOW_FORM: 'SHOW_FORM',
+  MEAL_INPUT: 'MEAL_INPUT',
+  CALORIES_INPUT: 'CALORIES INPUT',
 };
 
 export function showFormMsg(showForm) {
@@ -9,14 +13,43 @@ export function showFormMsg(showForm) {
   };
 }
 
+export function mealInputMsg(description) {
+  return {
+    type: MSGS.MEAL_INPUT,
+    description,
+  };
+}
+
+export function caloriesInputMsg(calories) {
+  return {
+    type: MSGS.CALORIES_INPUT,
+    calories,
+  };
+}
+
 function update(msg, model) {
-  const { SHOW_FORM } = MSGS;
+  const { SHOW_FORM, MEAL_INPUT, CALORIES_INPUT } = MSGS;
 
   switch (msg.type) {
     case SHOW_FORM: {
       const { showForm } = msg;
       return { ...model, showForm, description: '', calories: 0 };
     }
+    case MEAL_INPUT:
+      const { description } = msg;
+      return {
+        ...model,
+        description,
+      };
+    case CALORIES_INPUT:
+      const calories = R.pipe(
+        parseInt,
+        R.defaultTo(0)
+      )(msg.calories);
+      return {
+        ...model,
+        calories,
+      };
     default: {
       return model;
     }
