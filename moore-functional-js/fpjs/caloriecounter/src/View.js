03903213d7ccaf74f0b235707ad8d6d4c6b63c0a
klcantrell <cantrellkalalau@gmail.com>
2019-05-18 21:27:04 -0400

    moore-fp: add view functions for meals table

--- moore-functional-js/fpjs/caloriecounter/src/View.js
+++ moore-functional-js/fpjs/caloriecounter/src/View.js
@@ -1,5 +1,6 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
+import * as R from 'ramda';
 import {
   showFormMsg,
   mealInputMsg,
@@ -7,7 +8,22 @@ import {
   saveMealMsg,
 } from './Update';
 
-const { pre, div, h1, button, form, label, input } = hh(h);
+const {
+  pre,
+  div,
+  h1,
+  button,
+  form,
+  label,
+  input,
+  table,
+  tbody,
+  td,
+  tr,
+  th,
+  thead,
+  strong,
+} = hh(h);
 
 function fieldSet(labelText, inputValue, oninput) {
   return div([
@@ -72,10 +88,67 @@ function formView(dispatch, model) {
   );
 }
 
+function cell(tag, className, content) {
+  return tag({ className }, content);
+}
+
+const curriedCell = R.curry(cell);
+
+const basicRowCell = curriedCell(td);
+const tableHeaderCell = curriedCell(th);
+
+function mealRow(dispatch, className, meal) {
+  return tr({ className }, [
+    basicRowCell('pa2', meal.description),
+    basicRowCell('pa2 tr', meal.calories),
+    basicRowCell('pa2 tr', []),
+  ]);
+}
+
+function totalRow(meals) {
+  const total = R.compose(
+    String,
+    val => val || '',
+    R.reduce((acc, meal) => {
+      return acc + meal.calories;
+    }, 0)
+  );
+
+  return tr({ className: 'bt b' }, [
+    basicRowCell('pa2 tr', 'Total'),
+    basicRowCell('pa2 tr', total(meals)),
+    basicRowCell('', ''),
+  ]);
+}
+
+function mealsBody(dispatch, className, meals) {
+  const rows = R.map(R.partial(mealRow, [dispatch, 'stripe-dark']), meals);
+  const rowsWithTotal = [...rows, totalRow(meals)];
+  return tbody({ className }, rowsWithTotal);
+}
+
+const tableHeader = thead(
+  tr([
+    tableHeaderCell('pa2 tl', 'Meal'),
+    tableHeaderCell('pa2 tr', 'Calories'),
+    tableHeaderCell('', ''),
+  ])
+);
+
+function tableView(dispatch, meals) {
+  return meals.length === 0
+    ? div({ className: 'mv2 i black-50' }, 'No meals to display...')
+    : table({ className: 'mv2 w-100 collapse' }, [
+        tableHeader,
+        mealsBody(dispatch, '', meals),
+      ]);
+}
+
 function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Calorie Counter'),
     formView(dispatch, model),
+    tableView(dispatch, model.meals),
     pre(JSON.stringify(model, null, 2)),
   ]);
 }
