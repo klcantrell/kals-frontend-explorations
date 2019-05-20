29570e21a86180b7d623334aee34b119b6e9a495
klcantrell <cantrellkalalau@gmail.com>
2019-05-19 20:07:54 -0400

    max-pwa: create and store push subscription using VAPID

--- max-pwa/PWAGram/public/src/js/app.js
+++ max-pwa/PWAGram/public/src/js/app.js
@@ -45,9 +45,49 @@ function displayConfirmNotification() {
       ],
     };
     navigator.serviceWorker.ready.then(swReg => {
-      swReg.showNotification('Successfully subscribed (from SW)!', options);
+      swReg.showNotification('Successfully subscribed!', options);
     });
   }
+}
+
+function configurePushSub() {
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+  let reg;
+  navigator.serviceWorker.ready
+    .then(swReg => {
+      reg = swReg;
+      return swReg.pushManager.getSubscription();
+    })
+    .then(sub => {
+      if (sub === null) {
+        const vapidPublicKey =
+          'BBYbcjBB7fQYmdLLyxfnV-ILAx4XKSboWsWxi52KFSKQoBchnEYPWb8MDymOlmBcdmcjC_OUA9OWvPmRztdNN2M';
+        const convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+        return reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: convertedVapidPublicKey,
+        });
+      } else {
+      }
+    })
+    .then(newSub => {
+      return fetch('https://pwagram-d5dac.firebaseio.com/subscriptions.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify(newSub),
+      });
+    })
+    .then(res => {
+      if (res.ok) {
+        displayConfirmNotification();
+      }
+    })
+    .catch(console.log);
 }
 
 function askForNotificationPermission() {
@@ -56,12 +96,12 @@ function askForNotificationPermission() {
     if (result !== 'granted') {
       console.log('No notification permission granted!');
     } else {
-      displayConfirmNotification();
+      configurePushSub();
     }
   });
 }
 
-if ('Notification' in window) {
+if ('Notification' in window && 'serviceWorker' in navigator) {
   enableNotificationButtons.forEach(enableNotificationButton => {
     enableNotificationButton.style.display = 'inline-block';
     enableNotificationButton.addEventListener(
