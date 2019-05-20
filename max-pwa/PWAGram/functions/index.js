e15b285dea15b7f146a4cc14794d175a8f2f60a8
klcantrell <cantrellkalalau@gmail.com>
2019-05-19 20:44:33 -0400

    max-pwa: send push messages from server

--- max-pwa/PWAGram/functions/index.js
+++ max-pwa/PWAGram/functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({ origin: true });
+const webpush = require('web-push');
 
 const serviceAccount = require('./firebase-credentials.json');
 
@@ -22,6 +23,36 @@ exports.storePostData = functions.https.onRequest((req, res) => {
         image,
       })
       .then(() => {
+        webpush.setVapidDetails(
+          'mailto:cantrellkalalau@gmail.com',
+          'BBYbcjBB7fQYmdLLyxfnV-ILAx4XKSboWsWxi52KFSKQoBchnEYPWb8MDymOlmBcdmcjC_OUA9OWvPmRztdNN2M',
+          'CfN6DwANvkvykDhxiAA786gA6MvTgUzOKd_SOvw7dBM'
+        );
+        return admin
+          .database()
+          .ref('subscriptions')
+          .once('value');
+      })
+      .then(subscriptions => {
+        subscriptions.forEach(sub => {
+          const pushConfig = {
+            endpoint: sub.val().endpoint,
+            keys: {
+              auth: sub.val().keys.auth,
+              p256dh: sub.val().keys.p256dh,
+            },
+          };
+
+          webpush
+            .sendNotification(
+              pushConfig,
+              JSON.stringify({
+                title: 'New Post',
+                content: 'New Post Added!',
+              })
+            )
+            .catch(console.log);
+        });
         res.status(201).json({ message: 'Data stored', id });
       })
       .catch(err => {
