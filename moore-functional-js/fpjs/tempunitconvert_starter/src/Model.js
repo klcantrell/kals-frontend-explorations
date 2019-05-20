17dcd46045132daf4fae2274e51fd99b181810ed
klcantrell <cantrellkalalau@gmail.com>
2019-05-19 22:30:41 -0400

    moore-fp: [WIP] temperature conversion app

--- moore-functional-js/fpjs/tempunitconvert_starter/src/Model.js
+++ moore-functional-js/fpjs/tempunitconvert_starter/src/Model.js
@@ -1,5 +1,15 @@
+export const UNIT = {
+  CELSIUS: 'Celsius',
+  FAHRENHEIT: 'Fahrenheit',
+  KELVIN: 'Kelvin',
+};
+
 const initModel = {
-  
+  leftValue: 0,
+  leftUnit: UNIT.CELSIUS,
+  rightValue: 32,
+  rightUnit: UNIT.FAHRENHEIT,
+  sourceLeft: true,
 };
 
 export default initModel;
--- moore-functional-js/fpjs/tempunitconvert_starter/src/View.js
+++ moore-functional-js/fpjs/tempunitconvert_starter/src/View.js
@@ -1,17 +1,46 @@
 import * as R from 'ramda';
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
+import { UNIT } from './Model';
 
-const {
-  div,
-  h1,
-  pre,
-} = hh(h);
+const { div, h1, input, select, option, pre } = hh(h);
 
+function temperatureInput(value, oninput) {
+  return input(
+    {
+      oninput,
+    },
+    value
+  );
+}
+
+function unitSelect(selectedOption, oninput) {
+  const options = R.map(opt => {
+    if (opt === selectedOption) {
+      return option(
+        {
+          value: opt,
+          selected: true,
+          oninput,
+        },
+        opt
+      );
+    }
+    return option(
+      {
+        value: opt,
+        oninput,
+      },
+      opt
+    );
+  }, R.values(UNIT));
+  return select(options);
+}
 
 function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Temperature Unit Converter'),
+    unitSelect(UNIT.CELSIUS, () => console.log('sup')),
     pre(JSON.stringify(model, null, 2)),
   ]);
 }
