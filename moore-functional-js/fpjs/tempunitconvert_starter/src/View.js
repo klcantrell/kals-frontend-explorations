3ade30f663d20da90a9cb57fc528295a8c51b7a9
klcantrell <cantrellkalalau@gmail.com>
2019-05-20 20:57:57 -0400

    moore-fp: finish view functions design

--- moore-functional-js/fpjs/tempunitconvert_starter/src/View.js
+++ moore-functional-js/fpjs/tempunitconvert_starter/src/View.js
@@ -8,6 +8,7 @@ const { div, h1, input, select, option, pre } = hh(h);
 function temperatureInput(value, oninput) {
   return input(
     {
+      className: 'mb2',
       oninput,
     },
     value
@@ -16,31 +17,39 @@ function temperatureInput(value, oninput) {
 
 function unitSelect(selectedOption, oninput) {
   const options = R.map(opt => {
-    if (opt === selectedOption) {
       return option(
         {
           value: opt,
-          selected: true,
-          oninput,
+          selected: selectedOption === opt,
         },
         opt
       );
-    }
-    return option(
-      {
-        value: opt,
-        oninput,
-      },
-      opt
     );
   }, R.values(UNIT));
-  return select(options);
+  return select({ oninput }, options);
+}
+
+function formGroup(dispatch, data) {
+  return div({ className: 'flex flex-column w-30' }, [
+    temperatureInput(data.value, e => console.log(e.target.value)),
+    unitSelect(data.unit, e => console.log(e.target.value)),
+  ]);
+}
+
+const equalsDivider = div('=');
+
+function calculatorBody(dispatch, model) {
+  return div({ className: 'flex w-100 justify-around items-center' }, [
+    formGroup(dispatch, { value: model.leftValue, unit: model.leftUnit }),
+    equalsDivider,
+    formGroup(dispatch, { value: model.rightValue, unit: model.rightUnit }),
+  ]);
 }
 
 function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Temperature Unit Converter'),
-    unitSelect(UNIT.CELSIUS, () => console.log('sup')),
+    calculatorBody(dispatch, model),
     pre(JSON.stringify(model, null, 2)),
   ]);
 }
