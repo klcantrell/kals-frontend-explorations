47b67a5627a236c7726375b5745d77dd27020f62
klcantrell <cantrellkalalau@gmail.com>
2019-05-22 22:34:35 -0400

    moore-fp: add messages for left and right form elements

--- moore-functional-js/fpjs/tempunitconvert_starter/src/Update.js
+++ moore-functional-js/fpjs/tempunitconvert_starter/src/Update.js
@@ -1,7 +1,102 @@
 import * as R from 'ramda';
 
-function update (msg, model) {
-  return model;
+const toInt = R.pipe(
+  parseInt,
+  R.defaultTo(0)
+);
+
+const MSGS = {
+  LEFT_VALUE_INPUT: 'LEFT_VALUE_INPUT',
+  RIGHT_VALUE_INPUT: 'RIGHT_VALUE_INPUT',
+  LEFT_UNIT_OPTION: 'LEFT_UNIT_OPTION',
+  RIGHT_UNIT_OPTION: 'RIGHT_UNIT_OPTION',
+};
+
+function leftValueMsg(leftValue) {
+  return {
+    type: MSGS.LEFT_VALUE_INPUT,
+    leftValue,
+  };
 }
 
+function rightValueMsg(rightValue) {
+  return {
+    type: MSGS.RIGHT_VALUE_INPUT,
+    rightValue,
+  };
+}
+
+function leftUnitMsg(leftUnit) {
+  return {
+    type: MSGS.LEFT_UNIT_OPTION,
+    leftUnit,
+  };
+}
+
+function rightUnitMsg(rightUnit) {
+  return {
+    type: MSGS.RIGHT_UNIT_OPTION,
+    rightUnit,
+  };
+}
+
+function update(msg, model) {
+  switch (msg.type) {
+    case MSGS.LEFT_VALUE_INPUT: {
+      const { leftValue } = msg;
+      if (leftValue === '') {
+        return {
+          ...model,
+          sourceLeft: true,
+          leftValue: '',
+          rightValue: '',
+        };
+      }
+      const leftValueInt = toInt(leftValue);
+      return {
+        ...model,
+        sourceLeft: true,
+        leftValue: leftValueInt,
+      };
+    }
+    case MSGS.RIGHT_VALUE_INPUT: {
+      const { rightValue } = msg;
+      if (rightValue === '') {
+        return {
+          ...model,
+          sourceLeft: false,
+          leftValue: '',
+          rightValue: '',
+        };
+      }
+      const rightValueInt = toInt(rightValue);
+      return {
+        ...model,
+        sourceLeft: false,
+        rightValue: rightValueInt,
+      };
+    }
+    case MSGS.LEFT_UNIT_OPTION: {
+      const { leftUnit } = msg;
+      return {
+        ...model,
+        sourceLeft: true,
+        leftUnit,
+      };
+    }
+    case MSGS.RIGHT_UNIT_OPTION: {
+      const { rightUnit } = msg;
+      return {
+        ...model,
+        sourceLeft: false,
+        rightUnit,
+      };
+    }
+    default: {
+      return model;
+    }
+  }
+}
+
+export { leftValueMsg, rightValueMsg, leftUnitMsg, rightUnitMsg };
 export default update;
--- moore-functional-js/fpjs/tempunitconvert_starter/src/View.js
+++ moore-functional-js/fpjs/tempunitconvert_starter/src/View.js
@@ -2,20 +2,25 @@ import * as R from 'ramda';
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 import { UNIT } from './Model';
+import {
+  leftValueMsg,
+  rightValueMsg,
+  leftUnitMsg,
+  rightUnitMsg,
+} from './Update';
 
 const { div, h1, input, select, option, pre } = hh(h);
 
-function temperatureInput(value, oninput) {
-  return input(
-    {
-      className: 'mb2',
-      oninput,
-    },
-    value
-  );
+function temperatureInput(oninput, value) {
+  return input({
+    type: 'text',
+    className: 'mb2',
+    oninput,
+    value,
+  });
 }
 
-function unitSelect(selectedOption, oninput) {
+function unitSelect(oninput, selectedOption) {
   const options = R.map(opt => {
     return option(
       {
@@ -28,10 +33,10 @@ function unitSelect(selectedOption, oninput) {
   return select({ oninput }, options);
 }
 
-function formGroup(dispatch, data) {
+function formGroup(handleTemperatureInput, handleUnitSelect, data) {
   return div({ className: 'flex flex-column w-30' }, [
-    temperatureInput(data.value, e => console.log(e.target.value)),
-    unitSelect(data.unit, e => console.log(e.target.value)),
+    temperatureInput(handleTemperatureInput, data.value),
+    unitSelect(handleUnitSelect, data.unit),
   ]);
 }
 
@@ -39,9 +44,23 @@ const equalsDivider = div('=');
 
 function calculatorBody(dispatch, model) {
   return div({ className: 'flex w-100 justify-around items-center' }, [
-    formGroup(dispatch, { value: model.leftValue, unit: model.leftUnit }),
+    formGroup(
+      e => dispatch(leftValueMsg(e.target.value)),
+      e => dispatch(leftUnitMsg(e.target.value)),
+      {
+        value: model.leftValue,
+        unit: model.leftUnit,
+      }
+    ),
     equalsDivider,
-    formGroup(dispatch, { value: model.rightValue, unit: model.rightUnit }),
+    formGroup(
+      e => dispatch(rightValueMsg(e.target.value)),
+      e => dispatch(rightUnitMsg(e.target.value)),
+      {
+        value: model.rightValue,
+        unit: model.rightUnit,
+      }
+    ),
   ]);
 }
 
