a040a69ebfb6bdaa1b88e65d61c6f375d184fbe1
klcantrell <cantrellkalalau@gmail.com>
2019-05-27 21:37:22 -0400

    moore-fp: automatic temperature conversions

--- moore-functional-js/fpjs/tempunitconvert_starter/src/Update.js
+++ moore-functional-js/fpjs/tempunitconvert_starter/src/Update.js
@@ -1,4 +1,5 @@
 import * as R from 'ramda';
+import { UNIT } from './Model';
 
 const toInt = R.pipe(
   parseInt,
@@ -53,10 +54,17 @@ function update(msg, model) {
         };
       }
       const leftValueInt = toInt(leftValue);
+      const rightValueInt = convertTemperature(
+        model.leftUnit,
+        model.rightUnit,
+        leftValueInt
+      );
+
       return {
         ...model,
         sourceLeft: true,
         leftValue: leftValueInt,
+        rightValue: rightValueInt,
       };
     }
     case MSGS.RIGHT_VALUE_INPUT: {
@@ -70,26 +78,45 @@ function update(msg, model) {
         };
       }
       const rightValueInt = toInt(rightValue);
+      const leftValueInt = convertTemperature(
+        model.rightUnit,
+        model.leftUnit,
+        rightValueInt
+      );
+
       return {
         ...model,
         sourceLeft: false,
+        leftValue: leftValueInt,
         rightValue: rightValueInt,
       };
     }
     case MSGS.LEFT_UNIT_OPTION: {
       const { leftUnit } = msg;
+      const rightValueInt = convertTemperature(
+        leftUnit,
+        model.rightUnit,
+        model.leftValue
+      );
       return {
         ...model,
         sourceLeft: true,
         leftUnit,
+        rightValue: rightValueInt,
       };
     }
     case MSGS.RIGHT_UNIT_OPTION: {
       const { rightUnit } = msg;
+      const leftValueInt = convertTemperature(
+        rightUnit,
+        model.leftUnit,
+        model.rightValue
+      );
       return {
         ...model,
         sourceLeft: false,
         rightUnit,
+        leftValue: leftValueInt,
       };
     }
     default: {
@@ -97,6 +124,59 @@ function update(msg, model) {
     }
   }
 }
+
+function round(num) {
+  return Math.round(num * 10) / 10;
+}
+
+function convertTemperature(fromUnit, toUnit, fromValue) {
+  const convertFn = R.pathOr(R.identity, [fromUnit, toUnit], UnitConverstions);
+
+  return R.pipe(
+    convertFn,
+    round
+  )(fromValue);
+}
+
+function FtoC(temp) {
+  return (5 / 9) * (temp - 32);
+}
+
+function CtoF(temp) {
+  return (9 / 5) * temp + 32;
+}
+
+function KtoC(temp) {
+  return temp - 273.15;
+}
+
+function CtoK(temp) {
+  return temp + 273.15;
+}
+
+const FtoK = R.pipe(
+  FtoC,
+  CtoK
+);
+const KtoF = R.pipe(
+  KtoC,
+  CtoF
+);
+
+const UnitConverstions = {
+  [UNIT.CELSIUS]: {
+    [UNIT.FAHRENHEIT]: CtoF,
+    [UNIT.KELVIN]: CtoK,
+  },
+  [UNIT.FAHRENHEIT]: {
+    [UNIT.CELSIUS]: FtoC,
+    [UNIT.KELVIN]: FtoK,
+  },
+  [UNIT.KELVIN]: {
+    [UNIT.CELSIUS]: KtoC,
+    [UNIT.FAHRENHEIT]: KtoF,
+  },
+};
 
 export { leftValueMsg, rightValueMsg, leftUnitMsg, rightUnitMsg };
 export default update;
