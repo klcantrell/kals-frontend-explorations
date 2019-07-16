1ce571d6abad9296d4f2ed62e4e5a2ad8476e227
klcantrell <cantrellkalalau@gmail.com>
2019-07-15 21:42:46 -0400

    gatsby-eschweiler: fundamentals -> pages

---
+++ gatsby-eschweiler/test-blog/src/pages/info/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../../components/layout"
+
+const About = ({ location }) => (
+  <Layout location={location}>
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Consequat sunt minim enim mollit esse non elit consectetur esse esse
+        labore eiusmod. Nostrud ullamco nisi dolore reprehenderit adipisicing eu
+        est aute duis esse minim fugiat aliquip laboris. Laborum qui consectetur
+        magna ea.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default About
