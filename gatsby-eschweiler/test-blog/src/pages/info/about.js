b360e9e561de1d1d0c7fb986f640157f0abf8d83
klcantrell <cantrellkalalau@gmail.com>
2019-07-16 22:02:31 -0400

    gatsby-eschweiler: using components and using router components

--- gatsby-eschweiler/test-blog/src/pages/info/about.js
+++ gatsby-eschweiler/test-blog/src/pages/info/about.js
@@ -1,18 +1,22 @@
-import React from "react"
-import Layout from "../../components/layout"
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../../components/layout';
+import Greeting from '../../components/greeting';
 
 const About = ({ location }) => (
   <Layout location={location}>
     <div>
       <h1>About Page</h1>
+      <Link to="/">Home</Link>
       <p>
         Consequat sunt minim enim mollit esse non elit consectetur esse esse
         labore eiusmod. Nostrud ullamco nisi dolore reprehenderit adipisicing eu
         est aute duis esse minim fugiat aliquip laboris. Laborum qui consectetur
         magna ea.
       </p>
+      <Greeting greeting="Sup" name="my dude" />
     </div>
   </Layout>
-)
+);
 
-export default About
+export default About;
