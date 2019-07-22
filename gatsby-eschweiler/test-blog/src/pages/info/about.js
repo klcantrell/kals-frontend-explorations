535bdcc2268623684b39cfeedb5ded921e2dbd63
klcantrell <cantrellkalalau@gmail.com>
2019-07-21 22:20:36 -0400

    gatsby-eschweiler: using styled-components

--- gatsby-eschweiler/test-blog/src/pages/info/about.js
+++ gatsby-eschweiler/test-blog/src/pages/info/about.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../../components/layout2';
 import Greeting from '../../components/greeting';
-import styles from './about.module.css';
 import cloud from '../../images/cloud.png';
 
+const StyledUser = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto 12px auto;
+`;
+
+const Avatar = styled.img`
+  flex: 0 0 96px;
+  width: 96px;
+  margin: 0;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  margin-left: 18px;
+  padding: 12px;
+`;
+
+const Username = styled.div`
+  margin: 0 0 12px 0;
+`;
+
+const Excerpt = styled.p`
+  margin: 0;
+`;
+
 const User = ({ avatar, username, excerpt }) => (
-  <div className={styles.user}>
-    <img src={avatar} className={styles.avatar} alt="avatar" />
-    <div className={styles.description}>
-      <h2 className={styles.username}>{username}</h2>
-      <p className={styles.excerpt}>{excerpt}</p>
-    </div>
-  </div>
+  <StyledUser>
+    <Avatar src={avatar} />
+    <Description>
+      <Username>{username}</Username>
+      <Excerpt>{excerpt}</Excerpt>
+    </Description>
+  </StyledUser>
 );
 
 const About = ({ location }) => (
