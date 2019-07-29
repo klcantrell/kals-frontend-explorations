0d80e6f2ddcc348685e00c624286b9b8d986f5a3
klcantrell <cantrellkalalau@gmail.com>
2019-07-28 22:26:37 -0400

    gatsby-eschweiler: image plugins

--- gatsby-eschweiler/test-blog/src/components/layout2.js
+++ gatsby-eschweiler/test-blog/src/components/layout2.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 const ListLink = props => (
   <li
@@ -22,33 +23,46 @@ const Layout2 = ({ children }) => (
             title
           }
         }
+        file(name: { eq: "cloud" }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
       }
     `}
     render={data => (
-      <div
-        className="my-background"
-        style={{
-          margin: '0 auto',
-          maxWidth: 650,
-          padding: '0 1rem',
-        }}
-      >
-        <header style={{ marginBottom: '1.5rem' }}>
-          <h3 style={{ display: 'inline' }}>
-            {data.site.siteMetadata.title} Site
-          </h3>
-          <ul
-            style={{
-              listStyle: 'none',
-              float: 'right',
-            }}
-          >
-            <ListLink to="/">Home</ListLink>
-            <ListLink to="/info/about">About</ListLink>
-            <ListLink to="/blog-practice">Blog</ListLink>
-          </ul>
-        </header>
-        {children}
+      <div>
+        <Img
+          fluid={data.file.childImageSharp.fluid}
+          style={{ maxHeight: '400vh' }}
+        />
+        <div
+          className="my-background"
+          style={{
+            margin: '0 auto',
+            maxWidth: 650,
+            padding: '0 1rem',
+          }}
+        >
+          <header style={{ marginBottom: '1.5rem' }}>
+            <h3 style={{ display: 'inline' }}>
+              {data.site.siteMetadata.title} Site
+            </h3>
+            <ul
+              style={{
+                listStyle: 'none',
+                float: 'right',
+              }}
+            >
+              <ListLink to="/">Home</ListLink>
+              <ListLink to="/info/about">About</ListLink>
+              <ListLink to="/blog-practice">Blog</ListLink>
+            </ul>
+          </header>
+          {children}
+        </div>
       </div>
     )}
   ></StaticQuery>
