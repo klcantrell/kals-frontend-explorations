f66447e9dd75619f342a3e8cf625ecdf35d01627
klcantrell <cantrellkalalau@gmail.com>
2019-08-06 21:15:20 -0400

    gatsby-eschweiler: try to fix subproject commit

---
+++ contentful-blog/src/templates/blog-post-contentful.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+import Bio from '../components/bio';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { rhythm, scale } from '../utils/typography';
+
+class BlogPostContentfulTemplate extends React.Component {
+  render() {
+    const post = this.props.data.contentfulPost;
+    const siteTitle = this.props.data.site.siteMetadata.title;
+    const { previous, next } = this.props.pageContext;
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title={post.title}
+          description={post.description || post.subtitle}
+        />
+        <h1
+          style={{
+            marginTop: rhythm(1),
+            marginBottom: 0,
+          }}
+        >
+          {post.title}
+        </h1>
+        <p
+          style={{
+            ...scale(-1 / 5),
+            display: `block`,
+            marginBottom: rhythm(1),
+          }}
+        >
+          {post.createdAt}
+        </p>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: post.content.childContentfulRichText.html,
+          }}
+        />
+        <hr
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        />
+        <Bio />
+
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={`/${previous.slug}`} rel="prev">
+                ← {previous.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={`/${next.slug}`} rel="next">
+                {next.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </Layout>
+    );
+  }
+}
+
+export default BlogPostContentfulTemplate;
+
+export const pageQuery = graphql`
+  query ContentfulBlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    contentfulPost(slug: { eq: $slug }) {
+      title
+      createdAt
+      subtitle
+      author
+      slug
+      image {
+        file {
+          url
+        }
+      }
+      content {
+        childContentfulRichText {
+          html
+        }
+      }
+    }
+  }
+`;
